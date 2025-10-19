f2206173d04984d7315836d3dbc4f59d2e0b5103
Allana Karla <allanakarla1702@gmail.com>
2025-10-19 16:48:30 -0300

    :sparkles: feat: adds custom errors for transactions in the global error middleware

--- src/infra/exception/GlobalExceptionFilter.ts
+++ src/infra/exception/GlobalExceptionFilter.ts
@@ -2,11 +2,27 @@ import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
+  HttpException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import TransactionException from '@domain/exception/transaction/TransactionException';
 import DefaultExceptionJSON from './helper/DefaultExceptionJSON';
+import TransactionExceptionJSON from './helper/TransactionExceptionJSON';
 
-type IErrorResponse = { status: number; json: object | string };
+type TGenericErrorResponse = { status: number; json: object | string };
+
+const errorObject: Record<string, (error: Error) => TGenericErrorResponse> = {
+  TransactionException: (error: TransactionException) => TransactionExceptionJSON[error.message],
+  HttpException: (error: HttpException) => (
+    { status: error.getStatus(), json: error.getResponse() }
+  ),
+};
+
+const errorFilter = (exception: Error): TGenericErrorResponse => {
+  const errorFunction = errorObject[exception.constructor.name];
+  if (!errorFunction) return DefaultExceptionJSON.defaultError;
+  return errorFunction(exception);
+};
 
 @Catch()
 class GlobalExceptionFilter implements ExceptionFilter {
@@ -18,7 +34,13 @@ class GlobalExceptionFilter implements ExceptionFilter {
     console.log({ exception });
     console.log({ method: request.method, route: request.url });
 
-    const errorResponse:IErrorResponse = DefaultExceptionJSON.defaultError;
+    let errorResponse: TGenericErrorResponse;
+
+    if (exception instanceof Error) {
+      errorResponse = errorFilter(exception);
+    } else {
+      errorResponse = DefaultExceptionJSON.defaultError;
+    }
 
     return response.status(errorResponse.status).json(errorResponse.json);
   }
