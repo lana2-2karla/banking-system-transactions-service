a7666aa29e48b8909fbbadc7200e9857d8b9a752
Allana Karla <allanakarla1702@gmail.com>
2025-10-19 19:07:37 -0300

    :recycle: refactor: return of user capture by id can now be null

--- src/domain/case/user/IUserUseCase.ts
+++ src/domain/case/user/IUserUseCase.ts
@@ -1,7 +1,7 @@
 import IUser from '@domain/entity/user/IUser';
 
 interface IUserUseCase {
-  getById(id: string): Promise<IUser>
+  getById(id: string): Promise<IUser | null>
 }
 
 export default IUserUseCase;
