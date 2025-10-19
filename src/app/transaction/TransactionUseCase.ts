2704da5b3fe42474612888fa6165daa5666a2217
Allana Karla <allanakarla1702@gmail.com>
2025-10-19 16:32:45 -0300

    :sparkles: feat: transactions use case

---
+++ src/app/transaction/TransactionUseCase.ts
@@ -0,0 +1,40 @@
+import TTransactionUseCaseCreateInput 
+  from '@domain/case/transaction/input/TTransactionUseCaseCreateInput';
+import ITransactionUseCase from '@domain/case/transaction/ITransactionUseCase';
+import IUserUseCase from '@domain/case/user/IUserUseCase';
+import IUser from '@domain/entity/user/IUser';
+import TransactionException from '@domain/exception/transaction/TransactionException';
+import ITransactionRepository from '@domain/repository/transaction/ITransactionRepository';
+
+class TransactionUseCase implements ITransactionUseCase {
+  constructor(
+    private readonly _userUseCase: IUserUseCase,
+    private readonly _transactionRepository: ITransactionRepository,
+  ) {}
+
+  async create(input: TTransactionUseCaseCreateInput): Promise<void> {
+    const { senderId, receiverId, amount } = input;
+    
+    if (senderId === receiverId) throw new TransactionException('sameUserError');
+    if (amount <= 0) throw new TransactionException('invalidAmountError');
+
+    const userSender = await this._getUserOrThrow(senderId, 'sender');
+    await this._getUserOrThrow(receiverId, 'receiver');
+
+    if (userSender.balance < amount) throw new TransactionException('insufficientBalance');
+
+    await this._transactionRepository.createAtomic({ 
+      senderId,
+      receiverId,
+      amount,
+    });
+  }
+
+  private async _getUserOrThrow(id: string, role: 'sender' | 'receiver'): Promise<IUser> {
+    const user = await this._userUseCase.getById(id);
+    if (!user) throw new TransactionException(`${role}NotFound`);
+    return user;
+  }
+}
+
+export default TransactionUseCase;
