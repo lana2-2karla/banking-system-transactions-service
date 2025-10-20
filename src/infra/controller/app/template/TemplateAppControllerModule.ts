fac94e2e4d52b36e48b4509507c37f0c8fde3612
Allana Karla <allanakarla1702@gmail.com>
2025-10-19 23:03:45 -0300

    :sparkles: feat: implemnets transaction app controller module

--- src/infra/controller/app/template/TemplateAppControllerModule.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import TemplateAppController from './TemplateAppController';
-
-@Module({
-  imports: [],
-  controllers: [TemplateAppController],
-})
-
-export default class TemplateAppControllerModule {}
