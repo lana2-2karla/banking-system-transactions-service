5e7d1aeed930f0c8afcae8fffbdb8cc22065e925
Allana Karla <allanakarla1702@gmail.com>
2025-10-20 23:09:31 -0300

    :sparkles: feat: transaction with users dbo

---
+++ src/infra/repository/transaction/dbo/TransactionWithUsersDBO.ts
@@ -0,0 +1,35 @@
+import ETransactionStatus from '@domain/entity/transaction/ETransactionStatus';
+import ITransactionWithUsers from '@domain/entity/transaction/ITransactionWithUsers';
+import TTansactionUserDBStrict from '../interface/TTansactionUserDBStrict';
+
+class TransactionWithUsersDBO implements ITransactionWithUsers {
+  id: string;
+  senderId: string;
+  receiverId: string;
+  amount: string;
+  status: ETransactionStatus;
+  createdAt: Date;
+  sender: { id: string; name: string; balance: string };
+  receiver: { id: string; name: string; balance: string };
+
+  constructor(input: TTansactionUserDBStrict) {
+    this.id = input.id;
+    this.senderId = input.senderId;
+    this.receiverId = input.receiverId;
+    this.amount = input.amount.toString();
+    this.status = input.status as unknown as ETransactionStatus;
+    this.createdAt = input.createdAt;
+    this.sender = {
+      id: input.sender.id,
+      name: input.sender.name,
+      balance: input.sender.balance.toString(),
+    };
+    this.receiver = {
+      id: input.receiver.id,
+      name: input.receiver.name,
+      balance: input.receiver.balance.toString(),
+    };
+  }
+}
+
+export default TransactionWithUsersDBO;
