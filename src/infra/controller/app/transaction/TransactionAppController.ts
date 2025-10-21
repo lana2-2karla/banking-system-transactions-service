aa9df175295e603144bb7855debf35187b9d04a5
Allana Karla <allanakarla1702@gmail.com>
2025-10-21 15:48:02 -0300

    :sparkles: feat: removes access permission if the user sent is different from the authentication token

--- src/infra/controller/app/transaction/TransactionAppController.ts
+++ src/infra/controller/app/transaction/TransactionAppController.ts
@@ -1,7 +1,9 @@
 import TransactionUseCase from '@app/transaction/TransactionUseCase';
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import ITransactionUseCaseGetAllByUserIdOutput 
   from '@domain/case/transaction/output/ITransactionUseCaseGetAllByUserIdOutput';
+import AuthorizationException from '@domain/exception/authorization/AuthorizationException';
+import { Request } from 'express';
 import TransactionAppControllerCreateDTO from './dto/TransactionAppControllerCreateDTO';
 import TransactionGetAllByUserIdQueryDTO from './dto/TransactionGetAllByUserIdQueryDTO';
 
@@ -11,7 +13,13 @@ class TransactionAppController {
     private readonly _transactionUseCase: TransactionUseCase,
   ) { }
   @Post()
-  async create(@Body() body: TransactionAppControllerCreateDTO) {
+  async create(
+    @Body() body: TransactionAppControllerCreateDTO,
+      @Req() req: Request,
+  ): Promise<void> {
+    if (body.senderId !== req.user.id) {
+      throw new AuthorizationException('forbidden');
+    }
     await this._transactionUseCase.create(body);
   }
 
@@ -19,7 +27,11 @@ class TransactionAppController {
   async getAllByUserId(
     @Param('userId') userId: string,
       @Query() query: TransactionGetAllByUserIdQueryDTO,
+      @Req() req: Request,
   ): Promise<ITransactionUseCaseGetAllByUserIdOutput> {
+    if (userId !== req.user.id) {
+      throw new AuthorizationException('forbidden');
+    }
     const { page, limit } = query;
     return this._transactionUseCase.getAllByUserId(userId, page, limit);
   }
